775e74adf8e6382efd99f3b63c7170bad90136ed
Tho Le <37670626+leth02@users.noreply.github.com>
2021-11-01 17:15:29 -0500

    Add skeleton structure and placeholders for index, sign up, and sign in pages

---
+++ message_app/__init__.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request, render_template, session, redirect, url_for
+import requests
+import json
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    # if test config is passed, update app to use that config object
+    if test_config:
+        app.config.update(test_config)
+
+
+    # ===== HTML Pages =====
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+
+    @app.route("/messages", methods=["GET"])
+    def messages():
+        return render_template("messages.html")
+
+
+    @app.route("/signup", methods = ["GET"])
+    def user_signup():
+        return render_template("user_signup.html")
+
+
+    @app.route("/signin", methods = ["GET"])
+    def user_signin():
+        return render_template("user_signin.html")
+
+
+    # ===== JSON API endpoints =====
+
+    @app.route("/api/signup", methods = ["GET"])
+    def api_user_signup():
+        try:
+            params = request.form
+            username = params.get("username", "")
+            password = params.get("password", "")
+
+            if username == "" or password == "":
+                session["error"] = "Invalid username/password. Please try again."
+                return redirect(url_for("user_signup"))
+
+            # TODO connect to SQLite database and create a new account with the provided credentials
+            # if successful, redirect user to the app page
+            # otherwise, return JSON response containing the error
+            
+
+        except Exception as error:
+            return {"Error": "Bad request. " + str(error)}, 400
+
+
+    @app.route("/api/signin", methods = ["POST"])
+    def api_user_signin():
+        try:
+            params = request.form
+            username = params.get("username", "")
+            password = params.get("password", "")
+
+            if username == "" or password == "":
+                session["error"] = "Invalid username/password. Please try again."
+                return redirect(url_for("user_signup"))
+
+            # TODO connect to SQLite database and validate the provided credentials
+            # if successful, redirect user to the app page
+            # otherwise, return JSON response containing the error of invalid credentials
+            
+
+        except Exception as error:
+            return {"Error": "Bad request. " + str(error)}, 400
+
+    return app
---
+++ message_app/static/app.js
@@ -0,0 +1,3 @@
+(function() {
+    console.log("Placeholder for JavaScript scripts");
+})();
