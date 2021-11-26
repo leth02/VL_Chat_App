40bc0b3d2a0c7f60386e09f37532ce2667ab875f
Long Khuong <khuolo01@luther.edu>
2021-11-25 19:02:29 -0600

    update define and access the database

---
+++ tests/conftest.py
@@ -0,0 +1,43 @@
+import os
+import tempfile
+
+import pytest
+import os, sys
+
+# Path to message_app
+sys.path.append(os.getcwd()[:-5])
+
+from message_app import create_app
+from message_app.db.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'userdata.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
---
+++ tests/userdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (user_id, username, password, salt, email)
+VALUES
+    (1, "testUser1", "testPassword1", "testSalt1", "test1@gmail.com"),
+    (2, "testUser2", "testPassword2", "testSalt2", "test2@gmail.com");
