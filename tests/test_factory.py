31747d1d00fc2465ef296d432550911925ba0f1b
Long Khuong <khuolo01@luther.edu>
2021-12-13 18:05:16 -0600

    Update file test_factory passing name as a parameter for create_app()

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,4 +2,4 @@ from message_app import create_app
 
 def test_config():
     assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
+    assert create_app(__name__, {'TESTING': True}).testing
