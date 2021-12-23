f58cb175ac2f8dc9ebc5a96b870ed32e07e8a1a9
bhy2001 <88808104+bhy2001@users.noreply.github.com>
2021-12-23 16:13:55 -0600

    Add Sign In & Sign Up along with the css file

---
+++ message_app/templates/assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// =============== SIGN IN ===============
+// TODO: Write smthg here
+
+// =============== SIGN UP ===============
+// TODO: Write smthg here
+
+(function () {
+  console.log("Placeholder for JavaScript scripts");
+  // TODO: Write smthg here
+})();
