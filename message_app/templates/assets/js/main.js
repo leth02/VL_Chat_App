2d2f0054ee96b72a6569e4ba28fb09df48fa8ca0
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2021-12-23 22:24:47 -0600

    This is a draft of message app

---
+++ message_app/templates/assets/js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+// =============== SIGN IN ===============
+// TODO: Write smthg here
+
+// =============== SIGN UP ===============
+// TODO: Write smthg here
+
+
+(function() {
+    console.log("Placeholder for JavaScript scripts");
+// TODO: Write smthg here
+
+const toggle = document.getElementById('toggle');
+toggle.onclick = function () {
+  toggle.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
