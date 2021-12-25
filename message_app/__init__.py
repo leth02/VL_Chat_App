b74ff9fc46171f2adc81b671e41061693e73bc76
Long Khuong <khuolo01@luther.edu>
2021-12-25 12:20:53 -0600

    rebase new update

--- message_app/__init__.py
+++ message_app/__init__.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from flask import Flask, jsonify, request, render_template, session, redirect, url_for
 import requests
 import json
@@ -53,27 +54,58 @@ def create_app(test_config=None, name=__name__):
         return render_template("user_signup.html")
 
 
-    @app.route("/signin", methods=["GET"])
+    @app.route("/signin", methods=["GET", "POST"])
     def user_signin():
         return render_template("user_signin.html")
 
-
     # ===== JSON API endpoints =====
 
-    @app.route("/api/signup", methods=["GET"])
+    @app.route("/api/signup", methods=["POST"])
     def api_user_signup():
         try:
             params = request.form
             username = params.get("username", "")
             password = params.get("password", "")
+            email = params.get("email", "")
 
             if username == "" or password == "":
                 session["error"] = "Invalid username/password. Please try again."
                 return redirect(url_for("user_signup"))
 
-            # TODO connect to SQLite database and create a new account with the provided credentials
-            # if successful, redirect user to the app page
-            # otherwise, return JSON response containing the error
+            # Connect to the db
+            db_name = "VL_MESSAGES.db"
+            conn = sqlite3.connect(db_name)
+            c = conn.cursor()
+
+            # Create table users if not exist
+            c.execute("""CREATE TABLE IF NOT EXISTS users (
+                username text NOT NULL PRIMARY KEY,
+                password text NOT NULL,
+                salt text,
+                email text
+                )"""
+            )
+
+            #  Verify whether username is taken or not
+            c.execute("SELECT * FROM users WHERE username=:user_name", {'user_name': username})
+            userData = c.fetchone()
+            if userData:
+                session["error"] = "Username has been taken. Please try another one."
+                return redirect(url_for("user_signup"))
+
+            # Hash the password
+            salt = os.urandom(5)
+            password += str(salt)
+            h = sha256()
+            h.update(bytes(password, "utf8"))
+            password = h.hexdigest()
+
+            # Add user to the database
+            c.execute(f"INSERT INTO users VALUES (:username, :password, :salt, :email)", {"username": username, "password": password, "salt": salt, "email": email})
+            conn.commit()
+            c.close()
+
+            return redirect(url_for("index"))
 
         except Exception as error:
             return {"Error": "Bad request. " + str(error)}, 400
@@ -90,9 +122,34 @@ def create_app(test_config=None, name=__name__):
                 session["error"] = "Invalid username/password. Please try again."
                 return redirect(url_for("user_signup"))
 
-            # TODO connect to SQLite database and validate the provided credentials
-            # if successful, redirect user to the app page
-            # otherwise, return JSON response containing the error of invalid credentials
+            # Connect and fetch data from the users table
+            db_name = "VL_MESSAGES.db"
+            conn = sqlite3.connect(db_name)
+            c = conn.cursor()
+            with conn:
+                c.execute("SELECT password, salt FROM users WHERE username=:user_name", {'user_name': username})
+                userData = c.fetchone()
+            c.close()
+
+            # User not found
+            if not userData:
+                session["error"] = "Invalid username/password. Please try again."
+                return redirect(url_for("user_signup"))
+
+            correctPassword = userData[0]
+            salt = userData[1]
+            password = password + str(salt)
+
+            # Hash the password
+            h = sha256()
+            h.update(bytes(password, "utf8"))
+            password = h.hexdigest()
+
+            if password == correctPassword:
+                return redirect(url_for("index"))
+            else:
+                session["error"] = "Invalid username/password. Please try again."
+                return redirect(url_for("user_signup"))
 
         except Exception as error:
             return {"Error": "Bad request. " + str(error)}, 400
