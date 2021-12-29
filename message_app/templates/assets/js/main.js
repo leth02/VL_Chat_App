678be1d65293f169daec5c2a0ee1f0c8594cf917
bhy2001 <88808104+bhy2001@users.noreply.github.com>
2021-12-29 00:10:52 -0600

    Transitions and more styling. Will try to add Fetch APIin the next commit

--- message_app/templates/assets/js/main.js
+++ message_app/templates/assets/js/main.js
@@ -1,11 +1,25 @@
 "use strict";
 // =============== SIGN IN ===============
 // TODO: Write smthg here
-
+var signin_box = document.getElementById("signin-box");
+var signin = document.getElementById("signin");
+signin.onclick = function () {
+  if (signin_box.style.display === "none") {
+    signin_box.style.display = "block";
+  }
+  if (signup_box.style.display != "none") {
+    signup_box.style.display = "none";
+  }
+};
 // =============== SIGN UP ===============
 // TODO: Write smthg here
-
-(function () {
-  console.log("Placeholder for JavaScript scripts");
-  // TODO: Write smthg here
-})();
+var signup_box = document.getElementById("signup-box");
+var signup = document.getElementById("signup");
+signup.onclick = function () {
+  if (signup_box.style.display === "none") {
+    signup_box.style.display = "block";
+  }
+  if (signin_box.style.display != "none") {
+    signin_box.style.display = "none";
+  }
+};
