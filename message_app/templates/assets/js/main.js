cddb9d7334b5f784d72af4ec10450fd385657107
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2021-12-30 19:54:45 -0600

    chat convos parts

--- message_app/templates/assets/js/main.js
+++ message_app/templates/assets/js/main.js
@@ -5,42 +5,31 @@
 // =============== SIGN UP ===============
 // TODO: Write smthg here
 
+(function () {
+  console.log('Placeholder for JavaScript scripts');
+  // TODO: Write smthg here
 
-(function() {
-    console.log("Placeholder for JavaScript scripts");
-// TODO: Write smthg here
+  // ** Returns eventListener 
+  // @param {string} type - the type of eventListener
+  // @param {string} selector - the selector of element having events
+  // @param {function} callback - the callback function 
 
-const toggle = document.getElementById('toggle');
-toggle.onclick = function () {
-  toggle.classList.toggle('active');
-}
-
-
-
-
-
-
-
+  function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e => {
+      if (e.target.matches(selector)) callback(e)
+    })
+  }
 
 
+  let convosItem = document.getElementsByClassName('convos-item');
+  convosItem.addEventListener('click', function(e) {
+    // load convos 
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Right column of chat app
 
 
 
 })();
+
+
