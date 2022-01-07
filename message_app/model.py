6dd42297819a5a53284e84aef7a9cee2ea741385
Long Khuong <47405187+khuolo01@users.noreply.github.com>
2022-01-06 20:01:45 -0600

    Longkhuong/orm for conversations and messages tables (#16)
    * update orm for models messages and conversations
    
    * update comments for models.py
    
    * import new models in db.py
    
    * update flake8
    
    * update comments for models.py

--- message_app/model.py
+++ message_app/model.py
@@ -2,7 +2,18 @@
 from __future__ import annotations
 from message_app.db.db import DB as db
 import json
+import time
 from typing import Union, List
+
+# A join table for the many-to-many relationship between users and conversations tables.
+# Create a model for this table is unnecessary.
+users_conversations = db.Table(
+    "users_conversations",
+    db.Column("id", db.Integer, primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("conversation_id", db.Integer, db.ForeignKey("conversations.id")),
+    db.Column("seen", db.Boolean, default=False)
+)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -60,4 +71,101 @@ class User(db.Model):
     def get_last_user_id(cls) -> int:
         # Get last user ID
         id = User.query.order_by(User.id.desc()).first().id
+        return id
+
+class Conversations(db.Model):
+    __tablename__ = 'conversations'
+    id = db.Column(db.Integer, primary_key=True)
+    participants = db.relationship("User", secondary=users_conversations, backref="conversations", lazy=True)
+    messages = db.relationship("Messages", backref="conversations")
+    created_at = db.Column(db.Float, default=time.time())
+    last_message_id = db.Column(db.Integer) # This column is a foreignkey
+
+    # Return a json encoding of the conversation data
+    def to_json(self) -> str:
+        data = {
+            "id": self.id
+        }
+        return json.dumps(data)
+
+
+    #================Class Methods==================
+    @classmethod
+    def insert(cls, new_conversation: Conversations) -> None:
+        # Add a new user to the database
+        db.session.add(new_conversation)
+        db.session.commit()
+
+    @classmethod
+    def delete(cls, conversation_id: int) -> Union[Conversations, None]:
+        # Delete and return an user from the database. Return None if the user doesn't exist
+        conversation = Conversations.select(conversation_id)
+        if conversation:
+            db.session.delete(conversation)
+            db.session.commit()
+        return conversation
+
+    @classmethod
+    def select(cls, conversation_id: int) -> Union[Conversations, None]:
+        # Get an user from the database using username. Return None if the user doesn't exist
+        # The method selects only one user for now, but it CAN BE IMPROVED later on.
+        # TODO: Select multiple users with multiple conditions
+        conversation = Conversations.query.filter_by(id=conversation_id).first()
+        return conversation
+
+    @classmethod
+    def get_last_conversation_id(cls) -> int:
+        # Get last conversation ID
+        id = Conversations.query.order_by(Conversations.id.desc()).first().id
+        return id
+
+
+class Messages(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    content = db.Column(db.String(255))
+    created_at = db.Column(db.Integer, default=time.time())
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversations.id'))
+
+
+    # Return a json encoding of the user data
+    def to_json(self) -> str:
+        data = {
+            "id": self.id,
+            "sender_id": self.sender_id,
+            "content": self.content,
+            "conversation_id": self.conversation_id
+        }
+        return json.dumps(data)
+
+
+    #================Class Methods==================
+    @classmethod
+    def insert(cls, new_conversation: Messages) -> None:
+        # Add a new user to the database
+        db.session.add(new_conversation)
+        db.session.commit()
+
+    @classmethod
+    def delete(cls, message_id: int) -> Union[Messages, None]:
+        # Delete and return an user from the database. Return None if the user doesn't exist
+        message = Messages.select(message_id)
+        if message:
+            db.session.delete(message)
+            db.session.commit()
+        return message
+
+    @classmethod
+    def select(cls, message_id: int) -> Union[Messages, None]:
+        # Get an user from the database using username. Return None if the user doesn't exist
+        # The method selects only one user for now, but it CAN BE IMPROVED later on.
+        # TODO: Select multiple users with multiple conditions
+        message = Messages.query.filter_by(id=message_id).first()
+        return message
+
+    @classmethod
+    def get_last_message_id(cls) -> int:
+        # Get last message ID
+        id = Messages.query.order_by(Messages.id.desc()).first().id
         return id
---
+++ tests/test_models/test_messages.py
@@ -0,0 +1,38 @@
+import time
+from message_app.model import Messages
+import json
+
+class TestMessagesModel:
+    # Test for insertion, selection, and deletion of Messages model.
+    # Adding a valid message to the messages table.
+    # After successfully adding the new message, we delete that message.
+    # Selection is already included in the Messages.delete() function, so we don't have to
+    # create a test for it.
+    def test_basic_commands(self, test_db):
+        m = Messages(content="user3 sends a message to conversation2")
+
+        # Add the message to the database
+        Messages.insert(m)
+
+        assert m.id == Messages.get_last_message_id()
+
+        # Delete the same message from the database
+        deleted_message = Messages.delete(m.id)
+
+        assert m.id == Messages.get_last_message_id() + 1
+        assert deleted_message.id == m.id
+
+        # deleted_message should be None, for it has already been deleted
+        deleted_user = Messages.delete(m.id)
+        assert deleted_user == None
+
+    # Test Json
+    def test_message_to_json(self, test_db):
+        m = Messages.select(1)
+        test_data = {
+            "id": 1,
+            "sender_id": 1,
+            "content": "user1 sends a message to conversation1",
+            "conversation_id": 1
+        }
+        assert json.loads(m.to_json()) == test_data
