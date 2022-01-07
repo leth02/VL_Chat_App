086ffd8d1dca800de9fd3fe9b7f0d83bc3397595
Long Khuong <khuolo01@luther.edu>
2022-01-07 16:22:18 -0600

    first private message with test_conversation_id

--- message_app/send_message.py
+++ message_app/send_message.py
@@ -1,19 +1,40 @@
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask import Blueprint, render_template, session, redirect, url_for
 
-send_messages = Blueprint("send_messages", __name__, url_prefix="/messages")
+send_messages = Blueprint("send_messages", __name__)
 socketio = SocketIO(cors_allowed_origins='*')
 
-@send_messages.route("/", methods=["GET"])
+@send_messages.route("/messages", methods=["GET"])
 def messages():
-    if "user" not in session:
-        return redirect(url_for("user_sign_in.user_signin"))
-    else:
-        data = {
-            "username": session["username"]
-        }
-        return render_template("messages.html", username=session["username"])
+    # data = {
+    #     "username": session["username"]
+    # }
+    return render_template("messages.html", username=session["user"], conversation_id="test_conversation")
 
-@socketio.on('message_handle', namespace="/messages")
-def message_handle(data):
-    emit("message_handle", data, broadcast=True)
+@socketio.on("join", namespace="/messages")
+def joining(data):
+    # User join a conversation
+
+    username = data["username"]
+    conversation_id = data["conversation_id"]
+    join_room(conversation_id)
+
+    # Send a message that inform new user has join the conversation
+    emit("message_handler", {"message": username + " has join the conversation"}, room=conversation_id)
+
+@socketio.on("leave", namespace="/messages")
+def leaving(data):
+    # User leave a conversation
+    username = data["username"]
+    conversation_id = data["conversation_id"]
+    leave_room(conversation_id)
+
+    # Send a message that inform an user has left the conversation
+    emit("message_handler", {"message": username + "has left the conversation"})
+
+@socketio.on('message_handler', namespace="/messages")
+def message_handler(data):
+    username = data["username"]
+    message = data["message"]
+    conversation_id = data["conversation_id"]
+    emit("message_handler", {"username": username, "message": message}, room=conversation_id)
--- message_app/static/js/messages/socketio.js
+++ message_app/static/js/messages/socketio.js
@@ -1,22 +1,33 @@
-var socket = io('http://127.0.0.1:5000/messages');
+// document.addEventListener('DOMContentLoaded', () => {
+//     var socket = io('http://127.0.0.1:5000/messages');
 
-socket.on("connect", function(){
-    socket.emit("message_handle", {"username": `{{username}}`, "message": "has connected!"})
-});
+//     socket.on("connect", function(){
+//         socket.emit("message_handle", {"username": `{{username}}`, "message": "has connected!"})
+//     });
 
-socket.on("message_handle", function(data){
-    console.log(data)
-    var messages = document.getElementById("messages_container");
-    var p = document.createElement("p");
-    p.innerHTML = data.username + ": " + data.message;
-    messages.append(p);
-});
+//     socket.on("message_handle", function(data){
+//         console.log(data)
+//         var messages = document.getElementById("messages_container");
+//         var p = document.createElement("p");
+//         p.innerHTML = data.username + ": " + data.message;
+//         messages.append(p);
+//     });
 
-socket.on("disconnect", function(){
-    socket.emit("message_handle", "User disconnected")
-});
+//     socket.on("disconnect", function(){
+//         socket.emit("message_handle", "User disconnected")
+//     });
 
-function send_message(){
-    var text_field = document.getElementById("text_field");
-    socket.emit("message_handle", {"message": text_field.value, "username": `{{username}}`})
-}
+//     function send_message(){
+//         var text_field = document.getElementById("text_field");
+//         socket.emit("message_handle", {"message": text_field.value, "username": `{{username}}`})
+//     }
+// });
+
+// <!-- Server SocketIO JS -->
+//         <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js" integrity="sha512-q/dWJ3kcmjBLU4Qc47E4A9kTB4m3wuTY7vkFJDTZKjTs8jhyGQnaUrxa0Ytd0ssMZhbNua9hE+E7Qv1j+DyZwA==" crossorigin="anonymous"></script>
+        
+//         <!-- Custom Chat JS -->
+//         <script src="{{ url_for('static', filename='js/send_message.js') }}"></script>
+
+//         <!-- Socket IO -->
+//         <script src="{{ url_for('static', filename='js/socketio.js') }}"></script>
