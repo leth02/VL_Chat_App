a225995d8d1b7c90088f4a405eba99a773b9df8c
Long Khuong <47405187+khuolo01@users.noreply.github.com>
2022-01-17 20:46:42 -0600

    create react conversation components (#28)
    * create react conversation components
    
    * change from class component to functional component
    
    * add comments

---
+++ message_app_client/src/components/conversation.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+function ConversationCard(props){
+    // props.lastMessageID: int,
+    // props.otherParticipantStatus: String,
+    // props.conversationTitle: String,
+    // props.conversationID: int
+
+    const [lastMessageID, setLastMessageID] = useState(props.lastMessageID);
+    const [otherParticipantStatus, setOtherParticipantStatus] = useState(props.otherParticipantStatus);
+    const [conversationTitle, setConversationTitle] = useState(props.conversationTitle);
+    const [conversationID, setConversationID] = useState(props.conversationID);
+
+    // TODO: Add websocket here to update otherParticipantStatus and lastMessageID
+
+    return (
+        <div className="conversation-card__title" conversation_id={conversationID} other_participant_status={otherParticipantStatus}>
+            {conversationTitle} - {otherParticipantStatus}
+        </div>
+    )
+}
+
+function ConversationContainer(props){
+    // props.conversations: Array of objects containing conversation's data
+
+    let conversationCards = [];
+    let allConversations = props.conversations;
+    allConversations.forEach(conversation => {
+        conversationCards.push(<ConversationCard {...conversation} key={conversation.conversationID}/>)
+    })
+
+    return (
+        <div id="conversation-container">CONVERSATIONS {conversationCards}</div>
+    )
+}
+
+function ConversationPanel(props) {
+    // props.conversations: Array of objects containing conversation's data
+    // props.searchBar: WILL BE IMPLEMENTED LATER
+    return (
+        <div>
+            <ConversationContainer {...props}/>
+        </div>
+    )
+}
